43f2d34495eebda01c82b2cbd7970b251a54d312
Analulu <analuizarodriguesglinke@gmail.com>
2026-04-28 10:50:48 -0300

    Update Program.cs

--- mimitron/Program.cs
+++ mimitron/Program.cs
@@ -1,48 +1,55 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace primeiroprojeto
+namespace Mimi
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            sbyte a = 127;
-            int y = int.MaxValue;
-            int z = int.MinValue;
-            long x = 2147483648L; //<- por padrão coloca L no final
-            Console.WriteLine(a);
-            Console.WriteLine(y);
-            Console.WriteLine(z);
-            Console.WriteLine(x);
+            Funcionario f1 = new Funcionario();
+            Funcionario f2 = new Funcionario();
+            Console.WriteLine("Dados do primeiro funcionario: ");
+            Console.WriteLine("Nome: ");
+            f1.nome = Console.ReadLine();
+            Console.Write("Salario: ");
+            f1.salario = double.Parse(Console.ReadLine());
 
-            bool completo = false;
-            char genero = 'F';
-            char letra = '\u0041';
-            float f = 3.6f; //<-- por padrao coloar o f ao final
-            double f2 = 3.6;
+            Console.WriteLine("Dados do primeiro funcionario: ");
+            Console.WriteLine("Nome: ");
+            f2.nome = Console.ReadLine();
+            Console.Write("Salario: ");
+            f2.salario = double.Parse(Console.ReadLine());
 
-            Console.WriteLine(completo);
-            Console.WriteLine(genero);
-            Console.WriteLine(letra);
-            Console.WriteLine(f);
-            Console.WriteLine(f2);
+            double salario_medio = (f1.salario + f2.salario) / 2.0;
+            Console.WriteLine("Salario medio = " +salario_medio);
 
-            //-----------------------------------------
+            Console.WriteLine("primeiro funcionario: " +f1.nome);
+            Console.WriteLine("primeiro funcionario: " + f2.nome);
 
-            string nome;
-            nome = "Maria";
-            Console.WriteLine(nome);
-
-            object obj1 = "Aldenor Neto";
-            object obj2 = 1.0;
-
-            Console.Write(obj1); //<- nao pula linha
-            Console.WriteLine(obj2);
 
         }
     }
 }
+
+-----------Funcionario.cs
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mimi
+{
+    internal class Funcionario
+    {
+        public string nome;
+        public double salario;
+    }
+}
+
+
