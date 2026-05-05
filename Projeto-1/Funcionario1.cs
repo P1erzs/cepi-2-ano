14a9e9acbf3f1197219440e81d90a7a12a7ce823
Analulu <analuizarodriguesglinke@gmail.com>
2026-05-05 10:26:28 -0300

    Update Funcionario1.cs

--- Projeto-1/Funcionario1.cs
+++ Projeto-1/Funcionario1.cs
@@ -1,1 +1,17 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mimi
+{
+    internal class Funcionario
+    {
+        public string nome;
+        public double salario;
+    }
+}
+
+
 
